09e61392e42e3188ae056e56044b3be8cb59ed22
kash <kashiwa.electory@gmail.com>
2018-02-07 23:32:39 +0900

    Fix: Show details about heists/lobbies.

--- lua/platformmanager.lua
+++ lua/platformmanager.lua
@@ -3,7 +3,7 @@ core:module("PlatformManager")
 local set_rich_presence_discord_original = WinPlatformManager.set_rich_presence_discord
 
 function WinPlatformManager:set_rich_presence_discord(name)
-	set_rich_presence_discord_original(name)
+	set_rich_presence_discord_original(self, name)
 
     if name == "Idle" then
         Discord:set_status("ドリップコーヒー", "香風智乃のおパンツ")
